927e25de0799aadb25a0b6f005dcc5b33ce829ff
JokerLdg <dg600123@gmail.com>
2023-02-26 19:17:09 +0900

    1. 올바른 괄호 문제

---
+++ src/algorithm/stack/CorrectParentheses.java
@@ -0,0 +1,33 @@
+package algorithm.stack;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class CorrectParentheses {
+
+    public String solution(String s) {
+        String answer = "YES";
+        Stack<Character> stack = new Stack<>();
+
+        for (char c: s.toCharArray()) {
+            if ('(' == c) {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return "NO";
+                }
+                stack.pop();
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        CorrectParentheses main = new CorrectParentheses();
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.next();
+
+        System.out.println(main.solution(s));
+    }
+}
