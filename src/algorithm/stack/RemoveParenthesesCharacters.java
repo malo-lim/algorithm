4eb33aa4ceafae78e887056ef60b609b57f5e8f8
JokerLdg <dg600123@gmail.com>
2023-02-27 14:21:46 +0900

    괄호 문자 제거

---
+++ src/algorithm/stack/RemoveParenthesesCharacters.java
@@ -0,0 +1,34 @@
+package algorithm.stack;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class RemoveParenthesesCharacters {
+
+    public String solution(String s) {
+        StringBuilder answer = new StringBuilder();
+        Stack<Character> stack = new Stack<>();
+
+        for (char c: s.toCharArray()) {
+            if (c == ')') {
+                while (stack.pop() != '(');
+            } else {
+                stack.push(c);
+            }
+        }
+
+        for (char c: stack) {
+            answer.append(c);
+        }
+
+        return answer.toString();
+    }
+
+    public static void main(String[] args) {
+        RemoveParenthesesCharacters main = new RemoveParenthesesCharacters();
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.next();
+
+        System.out.println(main.solution(s));
+    }
+}
